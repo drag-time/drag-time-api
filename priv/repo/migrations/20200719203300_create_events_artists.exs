32e6033679d6d4f0043598cbfdd35512a7fc1349
Javier Aguilar <jvr.agx@gmail.com>
2020-07-19 16:52:28 -0600

    Update migration to include timestamps

--- priv/repo/migrations/20200719203300_create_events_artists.exs
+++ priv/repo/migrations/20200719203300_create_events_artists.exs
@@ -5,6 +5,8 @@ defmodule DragTimeApi.Repo.Migrations.CreateEventsArtists do
     create table(:events_artists) do
       add :event_id, references(:events)
       add :artist_id, references(:artists)
+
+      timestamps()
     end
 
     create unique_index(:events_artists, [:event_id, :artist_id])
