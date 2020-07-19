bc1cd8f5013dc5ad9994a37b5c6e6b212eceafc4
Javier Aguilar <jvr.agx@gmail.com>
2020-07-19 14:19:04 -0600

    Create seeds file

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,28 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias DragTimeApi.Repo
+alias DragTimeApi.Locations.Location
+
+Repo.insert! %Location{
+ name: "Tracks",
+ address: "3500 Walnut St",
+ city: "Denver",
+ state: "CO",
+ zip: "80205"
+}
+
+alias DragTimeApi.Repo
+alias DragTimeApi.Events.Event
+
+Repo.insert! %Event{
+ title: "DRAG NATION",
+ description: "Monsters and beauty queens unite for a one-of-a-kind drag experience. Along with Denver's hottest drag entertainers, we give you the glamour, filth, and horror that is the underground drag scene.",
+ labels: ["21+", "Cover Charge", "Drag King"],
+ image: "https://tracksdenver.com/wp-content/uploads/2019/06/02_28_DragNation3_Web.jpg",
+ date: ~D[2020-02-29],
+ start_time: ~T[22:00:00],
+ end_time: ~T[23:59:59],
+ cost: 10.00,
+ location_id: 1
+}
