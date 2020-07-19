a8179c90484ac93c60ba0a2e6d1ba984840066b6
Javier Aguilar <jvr.agx@gmail.com>
2020-07-19 15:17:20 -0600

    Add artists to routes

--- lib/drag_time_api_web/router.ex
+++ lib/drag_time_api_web/router.ex
@@ -9,6 +9,7 @@ defmodule DragTimeApiWeb.Router do
     pipe_through :api
     resources "/locations", LocationController, except: [:new, :edit]
     resources "/events", EventController, except: [:new, :edit]
+    resources "/artists", ArtistController, except: [:new, :edit]
   end
 
   # Enables LiveDashboard only for development
