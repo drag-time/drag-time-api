f8aa1ab6eed000b38bd5a21219c8b3023f98e5ee
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-07-26 23:50:13 -0600

    add create and index method for access resource

--- lib/drag_time_api_web/controllers/access_controller.ex
+++ lib/drag_time_api_web/controllers/access_controller.ex
@@ -1,14 +1,33 @@
 defmodule DragTimeApiWeb.AccessController do
   use DragTimeApiWeb, :controller
 
+  alias DragTimeApi.Accesses
+  alias DragTimeApi.Accesses.Access
+
   alias DragTimeApi.Repo
 
   action_fallback DragTimeApiWeb.FallbackController
 
   def index(conn, %{"access_code" => access_code}) do
-    if "1234" == access_code do
+    code = Phoenix.Token.verify(secret, "access", access_code)
+    if code == {:error, :invalid} do
+      send_resp(conn, 400, '{"error" : "incorrect access code"}')
+    else
       send_resp(conn, 200, '{"success" : "correct access code"}')
     end
-    send_resp(conn, 400, '{"error" : "incorrect access code"}')
   end
+
+  def create(conn, %{"access" => access_code_params}) do
+    code = Phoenix.Token.sign(secret, "access", access_code_params)
+    with {:ok, %Access{} = access} <- Accesses.create_access(%{"access_code" => code}) do
+      conn
+      |> put_status(:created)
+    end
+    send_resp(conn, 200, '{"success" : #{code}}')
+  end
+
+  defp secret do
+    "kjoy3o1zeidquwy1398juxzldjlksahdk3"
+  end
+
 end
